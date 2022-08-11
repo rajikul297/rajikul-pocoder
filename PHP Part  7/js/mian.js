0aaf36931d52e380b9e46cae9ace90fda985ceea
rajikul297 <111039297+rajikul297@users.noreply.github.com>
2022-08-11 11:51:12 +0600

    Add files via upload

---
+++ PHP Part  7/js/mian.js
@@ -0,0 +1,11 @@
+//part (3no) bx slider
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    
+//  Mobile Menu  mean jQuery Plagin
+    $('#main_menu').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+  });  
